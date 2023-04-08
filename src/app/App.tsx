7cbcbf78ffda2072421c6cbdaee05dd433db3b69
Ioseb Koplatadze <soso.kop@gmail.com>
2023-04-08 19:02:31 +0400

    background

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
 import { BlurFilter } from 'pixi.js';
 import { Stage, Container, Sprite, Text } from '@pixi/react';
 import { useMemo } from 'react';
+import background from '../assets/images/background.png';
+import { useResize } from '../shared/hooks/useResize';
 
 export const App = () => {
   const blurFilter = useMemo(() => new BlurFilter(4), []);
 
+  const { width, height } = useResize();
+
   return (
-    <Stage>
-      <Sprite
-        image="https://pixijs.io/pixi-react/img/bunny.png"
-        x={400}
-        y={270}
-        anchor={{ x: 0.5, y: 0.5 }}
-      />
+    <Stage height={height} width={width}>
+      <Sprite image={background} width={width} />
 
       <Container x={400} y={330}>
         <Text
