4d517bc61f99d8e4c2f0e9b55a419e270dc5fdae
Ioseb Koplatadze <soso.kop@gmail.com>
2023-04-08 18:35:43 +0400

    pixi/react

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import * as PIXI from 'pixi.js';
-
-const canvasEl = document.getElementById('canvas') as HTMLCanvasElement;
-const app = new PIXI.Application({ background: '#1099bb', view: canvasEl });
